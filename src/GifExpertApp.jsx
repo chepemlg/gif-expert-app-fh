81590deb672e583c1c196945ee4525090b71ca1d
Jose Lopez <jose.lg@alliedglobal.com>
2022-11-14 13:06:48 -0500

    Primer Commit Gif Expert App Curso React FH

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch']);
+
+  const onAddCategory = (newCategory) => {
+    // setCategories(categories => [...categories, 'Valorant']); // mi solución
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]); // solución Fernando
+  }
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+          <AddCategory 
+            // setCategories={setCategories}
+            onNewCategory={onAddCategory}
+          />
+
+          { 
+            categories.map(category => (
+                <GifGrid 
+                  key={category}
+                  category={category}
+                />
+              )
+            )
+          }
+    </>
+  )
+}
